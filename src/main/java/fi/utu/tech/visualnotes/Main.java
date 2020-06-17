e5130252977d1be98156daa4e771d010871b60ce
Mirva <mirva.tapola@gmail.com>
2020-06-17 17:26:53 +0300

    Lisätty ikoni.

--- src/main/java/fi/utu/tech/visualnotes/Main.java
+++ src/main/java/fi/utu/tech/visualnotes/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -15,8 +16,15 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/visualnotes.fxml"));
 
         Scene scene = new Scene(root);
-        //scene.getStylesheets().add("style.css");
 
+        /*
+        Icon made by "http://www.freepik.com/"
+         */
+
+
+        stage.getIcons().add(new Image("paint-brush-16.png"));
+        stage.getIcons().add(new Image("paint-brush-24.png"));
+        stage.getIcons().add(new Image("paint-brush-32.png"));
         stage.setTitle("Visual notes");
         stage.setScene(scene);
         stage.show();
